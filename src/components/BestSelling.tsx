609a73b677bfe6036feac6e69f746d8b99c157a3
arthuracmm <arthurcesarmarcelino@gmail.com>
2025-07-05 21:55:19 -0300

    reajustando a header para ja compor o endereço e sua alteração

--- src/components/BestSelling.tsx
+++ src/components/BestSelling.tsx
@@ -1,7 +1,14 @@
 export default function BestSelling() {
     return (
-        <div>
-            <h1>Best Selling</h1>
+            <div className="flex flex-col gap-2 overflow-x-auto whitespace-nowrap w-full py-2 px-1 scrollbar-hide pl-2">
+                <div className="flex w-full justify-between">
+                    <h1 className="text-lg font-bold">Item mais Vendido</h1>
+                </div>
+                <div className="flex flex-row gap-2">
+                    <div className="flex flex-col gap-2">
+                        <img src="https://rseat.pics/" alt="Item" />
+                    </div>
+                </div>
         </div>
     )
 }
--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,29 +1,27 @@
-import { ArrowRight, CakeSlice } from "lucide-react"
+import { useState } from "react"
 
 export default function Categories() {
 
     const categories = [
         {
-            name: "Vulcões",
-            background: "bg-red-200"
+            name: "Bolos1"
         },
         {
-            name: "Bolos",
-            background: "bg-blue-200"
+            name: "Bolos2"
         },
         {
-            name: "Salgados",
-            background: "bg-green-200"
+            name: "Bolos3"
         },
         {
-            name: "Sobremesa",
-            background: "bg-yellow-200"
+            name: "Bolos4"
         },
         {
-            name: "Bebida",
-            background: "bg-purple-200"
+            name: "Bolos5"
         },
     ]
+
+    const [CategorySelected, setCategorySelected] = useState('')
+
     return (
         <div className="w-full flex flex-col gap-2 ">
             <div className="flex flex-col gap-2 overflow-x-auto whitespace-nowrap w-full py-2 px-1 scrollbar-hide pl-2">
@@ -32,9 +30,8 @@ export default function Categories() {
                 </div>
                 <div className="flex flex-row gap-2">
                     {categories.map((category, idx) => (
-                        <a key={idx} href="#" className={`flex-shrink-0 cursor-pointer text-center w-25 h-30 flex flex-col items-center overflow-hidden justify-center rounded-xl relative ${category.background}`}>
+                        <a key={idx} href="#" className={`cursor-pointer text-center w-20 py-2 flex flex-shrink-0 flex-col items-center overflow-hidden justify-center rounded-xl ${CategorySelected === category.name ? 'bg-red-300 text-white' : 'border-1 border-red-300 text-red-400'}`} onClick={() => setCategorySelected(category.name)}>
                             <p>{category.name}</p>
-                            <CakeSlice size={100} className="absolute bottom-[-45px]" />
                         </a>
                     ))}
 
